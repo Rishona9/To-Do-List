72a14f8c91ba490a903437a493fc66ab69228efd
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-07-21 10:16:29 -0400

    Added attachRemoveButton function to each list item

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const btnCreate = document.querySelector(".btn-main");
 const btnToggle = document.querySelector(".btn-toggle");
 const taskList = document.querySelector(".list-container ul");
+const btnRemove = document.querySelector(".btn-remove");
+const listItems = taskList.children;
+
+function attachRemoveButton(li) {
+  let remove = document.createElement("button");
+  remove.className = "btn-remove";
+  remove.textContent = "Remove";
+  li.appendChild(remove);
+}
+for (i = 0; i < listItems.length; i++) {
+  attachRemoveButton(listItems[i]);
+}
+
+taskList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const button = event.target;
+    const li = button.parentNode;
+    li.remove();
+  }
+});
 
 btnCreate.addEventListener("click", () => {
   let ul = document.getElementsByTagName("ul")[0];
   const input = document.querySelector(".input-main");
   let li = document.createElement("li");
   li.textContent = input.value;
+  attachRemoveButton(li);
   ul.appendChild(li);
   input.value = "";
 });
